5389e265afdf07689f3d736090243b1681c5179f
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-18 11:20:44 +0300

    feat: implement lazy loading for feature modules and add core module

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,5 +3,35 @@ import { Routes, RouterModule } from '@angular/router';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-
+  {
+    path: '',
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('./features/landing/landing.module').then((m) => m.LandingModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./features/auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'app',
+    canActivate: [authGuard],
+    children: [
+      {
+        path: 'products',
+        loadChildren: () =>
+          import('./features/product/product.module').then(
+            (m) => m.ProductModule
+          ),
+      },
+      {
+        path: 'categories',
+        loadChildren: () =>
+          import('./features/category/category.module').then(
+            (m) => m.CategoryModule
+          ),
+      },
+    ],
+  },
 ];
