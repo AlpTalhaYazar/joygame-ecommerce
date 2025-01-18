cecca106ef571c38fc73f819a1f78be787cd8b83
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-18 19:15:37 +0300

    feat: enhance login component with form and styling

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {NzCardComponent} from 'ng-zorro-antd/card';
+import {NzFormControlComponent, NzFormDirective, NzFormItemComponent} from 'ng-zorro-antd/form';
+import {FormsModule} from '@angular/forms';
+import {NzInputDirective, NzInputGroupComponent} from 'ng-zorro-antd/input';
+import {NzButtonComponent} from 'ng-zorro-antd/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    NzCardComponent,
+    NzFormDirective,
+    FormsModule,
+    NzFormItemComponent,
+    NzFormControlComponent,
+    NzInputGroupComponent,
+    NzInputDirective,
+    NzButtonComponent,
+    RouterLink
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  onSubmit() {
+    // Implement login logic here
+    console.log('Login attempt', this.username, this.password);
+  }
 }
