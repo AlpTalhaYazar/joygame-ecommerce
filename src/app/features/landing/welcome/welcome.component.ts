c6965cf77221cf953c3b64194b40cbc8c02c00b3
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-18 19:15:45 +0300

    feat: enhance welcome component with layout, content, and navigation

--- src/app/features/landing/welcome/welcome.component.ts
+++ src/app/features/landing/welcome/welcome.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NzDividerComponent } from 'ng-zorro-antd/divider';
+import { NzColDirective, NzRowDirective } from 'ng-zorro-antd/grid';
+import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { NzIconDirective } from 'ng-zorro-antd/icon';
+import {NzListComponent, NzListItemComponent} from 'ng-zorro-antd/list';
+import { NzTypographyComponent } from 'ng-zorro-antd/typography';
+import { NzCardComponent } from 'ng-zorro-antd/card';
 
 @Component({
   selector: 'app-welcome',
-  imports: [],
+  standalone: true,
+  imports: [
+    NzDividerComponent,
+    NzRowDirective,
+    NzColDirective,
+    NzButtonComponent,
+    NzIconDirective,
+    NzListItemComponent,
+    NzTypographyComponent,
+    NzCardComponent,
+    NzListComponent,
+  ],
   templateUrl: './welcome.component.html',
-  styleUrl: './welcome.component.scss'
+  styleUrl: './welcome.component.scss',
 })
 export class WelcomeComponent {
+  constructor(private router: Router) {}
 
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
