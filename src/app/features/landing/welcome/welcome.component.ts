a1a0c292c888a8e0fa7fc54ef5998347f3c6b80d
Alp Talha Yazar <AlpTalhaYazar@gmail.com>
2025-01-20 03:12:51 +0300

    feat: enhance welcome component to display user names and conditional navigation buttons based on login state

--- src/app/features/landing/welcome/welcome.component.ts
+++ src/app/features/landing/welcome/welcome.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { NzDividerComponent } from 'ng-zorro-antd/divider';
-import { NzColDirective, NzRowDirective } from 'ng-zorro-antd/grid';
-import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
 import { NzIconDirective } from 'ng-zorro-antd/icon';
-import {NzListComponent, NzListItemComponent} from 'ng-zorro-antd/list';
-import { NzTypographyComponent } from 'ng-zorro-antd/typography';
 import { NzCardComponent } from 'ng-zorro-antd/card';
+import { Component, OnInit } from '@angular/core';
+import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { NzDividerComponent } from 'ng-zorro-antd/divider';
+import { NzTypographyComponent } from 'ng-zorro-antd/typography';
+import { NzColDirective, NzRowDirective } from 'ng-zorro-antd/grid';
+import { NzListComponent, NzListItemComponent } from 'ng-zorro-antd/list';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-welcome',
@@ -21,14 +23,28 @@ import { NzCardComponent } from 'ng-zorro-antd/card';
     NzTypographyComponent,
     NzCardComponent,
     NzListComponent,
+    NgIf,
+    RouterLink,
   ],
   templateUrl: './welcome.component.html',
   styleUrl: './welcome.component.scss',
 })
-export class WelcomeComponent {
-  constructor(private router: Router) {}
+export class WelcomeComponent implements OnInit {
+  isUserLoggedIn: boolean = false;
+  firstName: string = '';
+  lastName: string = '';
 
-  navigateToLogin() {
-    this.router.navigate(['/login']);
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.isUserLoggedIn = this.authService.isUserLoggedIn();
+
+    if (this.isUserLoggedIn) {
+      const currentUser = this.authService.getCurrentUser();
+      if (currentUser) {
+        this.firstName = currentUser.firstName;
+        this.lastName = currentUser.lastName;
+      }
+    }
   }
 }
